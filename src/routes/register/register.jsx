6165e8fdb253cc532670d3d10e8b553ff2ccd973
Safak <1486366+safak@users.noreply.github.com>
2024-03-14 12:33:05 +0100

    Create register.jsx

---
+++ src/routes/register/register.jsx
@@ -0,0 +1,24 @@
+import "./register.scss";
+import { Link } from "react-router-dom";
+
+function Register() {
+  return (
+    <div className="register">
+      <div className="formContainer">
+        <form>
+          <h1>Create an Account</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button >Register</button>
+          <Link to="/login">Do you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Register;
